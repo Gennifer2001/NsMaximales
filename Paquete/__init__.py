ff486462e216548c70a873b7dd030b8373e56f6f
Gennifer <gennifermontanorodriguez@gmail.com>
2024-02-28 21:52:59 -0500

    added __init__.py

--- Paquete/__init__.py
+++ Paquete/__init__.py
@@ -1,5 +1,5 @@
- __titulo__= 'NsMaximales'
+__titulo__= 'NsMaximales'
 __Autores__= 'Yeimi Rodríguez y Gennifer Montaño'
 __versión__= '0.1'
-__correo__ = "gennifermontanorodriguez@gmail.com o yeimir000@gmail.com'
+__correo__ = "gennifermontanorodriguez@gmail.com o yeimir0000@gmail.com"
 __descripción__='NsMaximales se encarga de calcular los n-subárboles maximales y apartir de ellos obtener una base topológica'
