d3d466958196344d4d8087b797f35ac2a9029743
Gennifer <gennifermontanorodriguez@gmail.com>
2024-02-28 20:44:52 -0500

    Added convertir_a_arbol.py

---
+++ Paquete/Puntos_topologicos.py
@@ -0,0 +1,65 @@
+class puntos_topologicos:
+
+  def exterior(Base, conjunto,df):
+    Ext = []
+
+    for i in Base:
+        if not any(elem in conjunto for elem in i):
+            Ext.extend(i)
+
+    Ext_enteros = [int(elemento) for elemento in Ext]
+    tabla = df.loc[Ext_enteros]
+
+    print(f"Ext({'A'})= {Ext}")
+    return tabla
+  
+  def interior(Base,conjunto,df):
+    Int = []
+
+    for i in Base:
+        if all(elem in conjunto for elem in i):
+            Int.extend(i)
+
+    Int_enteros = [int(elemento) for elemento in Int]
+    tabla = df.loc[Int_enteros]
+
+    print(f"Int({'A'}) = {Int}")
+    return tabla
+  def adherencia(Base,A,df):
+        Adh = []
+
+        for i in Base:
+          if any(elem in A for elem in i):
+           Adh.extend(i)
+
+        Adh_enteros = [int(elemento) for elemento in Adh]
+        tabla = df.loc[Adh_enteros]
+
+        print(f"{'A'}'= {Adh}")
+        return tabla
+  def limite(Base,A,df):
+   Lim = []
+
+   for i in Base:
+     for m in i:
+       if any(elem in A for elem in i if elem != m):
+         Lim.append(m)
+
+   Lim_enteros = [int(elemento) for elemento in Lim]
+   tabla = df.loc[Lim_enteros]
+
+   print(f"{'A'}'= {Lim}")
+   return tabla
+  
+  def frontera(Base,A,df):
+   Fr = []
+
+   for i in Base:
+       if any(elem in A for elem in i) and any(elem not in A for elem in i):
+           Fr.extend(i)
+
+   Fr_enteros = [int(elemento) for elemento in Fr]
+   tabla = df.loc[Fr_enteros]
+
+   print(f"Fr({'A'})= {Fr}")
+   return tabla
