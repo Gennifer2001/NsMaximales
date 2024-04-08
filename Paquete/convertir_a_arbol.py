480fc5b6d397abb212d605763a9499da7bae7b39
Gennifer2001 <148257216+Gennifer2001@users.noreply.github.com>
2024-04-08 18:53:30 -0500

    Update convertir_a_arbol.py

--- Paquete/convertir_a_arbol.py
+++ Paquete/convertir_a_arbol.py
@@ -1,28 +1,4 @@
-class convertir_a_arbol:
-   def get_newick(node, parent_dist, leaf_names, newick='') -> str:
-    """
-    Convert sciply.cluster.hierarchy.to_tree()-output to Newick format.
-
-    :param node: output of sciply.cluster.hierarchy.to_tree()
-    :param parent_dist: output of sciply.cluster.hierarchy.to_tree().dist
-    :param leaf_names: list of leaf names
-    :param newick: leave empty, this variable is used in recursion.
-    :returns: tree in Newick format
-    """
-    if node.is_leaf():
-        return "%s:%.2f%s" % (leaf_names[node.id], parent_dist - node.dist, newick)
-    else:
-        if len(newick) > 0:
-            newick = "):%.2f%s" % (parent_dist - node.dist, newick)
-        else:
-            newick = ");"
-        newick = get_newick(node.get_left(), node.dist, leaf_names, newick=newick)
-        newick = get_newick(node.get_right(), node.dist, leaf_names, newick=",%s" % (newick))
-        newick = "(%s" % (newick)
-        return newick
-    
-
-   def convertir_a_Tree(dendo, leaf_names):
+def convertir_a_Tree(dendo, leaf_names):
       from scipy.cluster import hierarchy
       """
       Convierte la salida de scipy.cluster.hierarchy.to_tree a un objeto ete3.Tree.
