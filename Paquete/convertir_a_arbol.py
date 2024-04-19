14b9ff9a79cf6017bba4e366d2c67dfc03dc4f6e
Gennifer2001 <148257216+Gennifer2001@users.noreply.github.com>
2024-04-18 19:30:50 -0500

    Update convertir_a_arbol.py

--- Paquete/convertir_a_arbol.py
+++ Paquete/convertir_a_arbol.py
@@ -28,6 +28,7 @@ class convertir_a_arbol:
       Convierte la salida de scipy.cluster.hierarchy.to_tree a un objeto ete3.Tree.
       """
       tree1 = hierarchy.to_tree(dendo, False)
+      from Paquete.get_netwick import get_newick
       newick_tree1 = get_newick(tree1, tree1.dist, leaf_names)
       tree = ete3.Tree(newick_tree1)
 
