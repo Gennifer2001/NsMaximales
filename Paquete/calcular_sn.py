c80c2342b1ca043af51a7f55a0a4e179cd86c60a
Gennifer2001 <148257216+Gennifer2001@users.noreply.github.com>
2024-07-02 17:23:31 -0500

    Update calcular_sn.py

--- Paquete/calcular_sn.py
+++ Paquete/calcular_sn.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
+
 def calcular_sn(maximales):
     """
-    Calcula y devuelve la lista Sn a partir de la lista de n-subárboles maximales.
+    Calcula y devuelve la lista Sn a partir de la lista de n-subárboles maximales y grafica los resultados.
     """
     Sn = []  # Inicializa la lista para los valores de Sn
 
@@ -10,4 +12,24 @@ def calcular_sn(maximales):
             Sj *= len(subarbol)  # Actualiza Sj con el valor de la operación
         Sn.append((n, Sj))
 
+    # Limitar la lista de tuplas a las primeras 25
+    lista_tuplas = Sn[:25]
+
+    # Extraer las coordenadas x e y de las tuplas
+    x_coords = range(1, len(lista_tuplas) + 1)  # Valores consecutivos de 1 en 1
+    y_coords = [tupla[1] for tupla in lista_tuplas]
+    # Graficar los puntos y conectarlos con líneas
+    plt.plot(x_coords, y_coords, marker='o', linestyle='-')
+    # Establecer los valores de las etiquetas del eje x
+    plt.xticks(x_coords)
+    # Etiquetas de los ejes
+    plt.xlabel('Valor de n')
+    plt.ylabel('Valor de Sn')
+    # Título del gráfico
+    plt.title('Variación de Sn')
+    # Mostrar el gráfico
+    plt.grid(True)
+    plt.show()
+
     return Sn
+
